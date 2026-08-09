09cd9f795aa3b07efdf306a8ce81807ab45d8030
Saketh <msakethsaketh27@gmail.com>
2026-08-09 18:24:10 +0530

    routing implemented by useNavigate

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 function Home() {
+    const navigate=useNavigate();
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home page</h1>
+        <button onClick={()=>navigate('/login')}>Login</button>
+        <button onClick={()=>navigate('/register')}>Register</button>
+    </div>
   )
 }
 
