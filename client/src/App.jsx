2179cf12615efa2e30bb230ee284db55d3a8b8d4
Saketh <msakethsaketh27@gmail.com>
2026-08-09 18:13:42 +0530

    Routing implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Home'
+import Login from './Login'
+import Register from './Register'
 function App() {
-  
-
   return (
-    <div>
-      learn authentication
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
     
   )
 }
