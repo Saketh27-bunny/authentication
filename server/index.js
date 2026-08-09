b35e50b6bc74d3147669af6ba0d1170e1c64de94
Saketh <msakethsaketh27@gmail.com>
2026-08-10 04:14:49 +0530

    MONGODB connection

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
+import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-
 app.use(cors()); 
-
 // Enables cross-origin requests from React
-
 app.use(express.json()); 
 
-// Parses incoming JSON request bodies
+// connect to mongoDB
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("DB as connected"))
+.catch((err)=> console.log('MongoDB connection error',err));
+
 
 // Base Test Route
 
